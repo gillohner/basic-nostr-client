fac0e5a063146e7f4d6626c752670c873eb600bd
Gil Lohner <gil.lohner@indu40.com>
2023-06-21 22:09:32 +0200

    set up pool

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
-import React from 'react';
+import 'websocket-polyfill'
+import { SimplePool } from 'nostr-tools';
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+export const Relays = [
+  "wss://relay.damus.io",
+  "wss://nostr.drss.io",
+]
+
 function App() {
+  const [pool, setPool] = useState<SimplePool | null>(null);
+
+  // setup a relay pool
+  useEffect(() => {
+    const _pool = new SimplePool();
+    setPool(_pool);
+
+    return () => {
+      _pool.close(Relays);
+    }
+  }, []);
+
+  // subscribe to some events
+  useEffect(() => {
+    if (!pool) return;
+
+    const sub = pool.sub(Relays, [{
+      kinds: [1],
+      limit: 69,
+    }])
+
+    sub.on('event', (event) => {
+      console.log(event);
+    })
+  }, [pool]);
+
   return (
     <div className="App">
       <header className="App-header">
